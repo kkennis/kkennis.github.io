0d6961640b79ac9044f0849df6877a44a5fd4b4b
kkennis <kevinvkennis@gmail.com>
2016-01-14 21:59:21 -0800

    Get dot drawing working

--- js/app.js
+++ js/app.js
@@ -17,8 +17,25 @@ function drawDots(section){
   var pageWidth = $('.container').width();
   var tocItems = $(section).children('.toc-item');
 
-  $.each(tocItems, function(item){
+  $.each(tocItems, function(index, item){
+    var title = $(item).children('.toc-title');
+    var date = $(item).children('.toc-date');
 
-  })
+    var itemWidth = pageWidth - title.width() - date.width();
 
+    var numDots = Math.floor(itemWidth / 8.82813) - 1;
+    var dots = title.next().width(itemWidth);
+
+    var count = 0;
+
+    if (dots.is(':empty')){
+      var dotPainter = setInterval(function(){
+        dots.append($('<span> . </span>'));
+        count++;
+        if (count === numDots) clearInterval(dotPainter);
+      });
+    }
+
+  });
 }
+
